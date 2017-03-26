59d372d20e8b5bb084ad8dfa3538e31c33bcf895
Dan Wahlin <danwahlin@Dans-MacBook-Pro.local>
2017-03-26 10:16:48 -0700

    Fixed ngtools/aotplugin 1.3 bug that caused issue on Windows.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,26 @@ const ngToolsWebpack = require('@ngtools/webpack'),
       webpack = require('webpack'),
       path = require('path');
 
+var aotPlugin = new ngToolsWebpack.AotPlugin({
+    tsConfigPath: "./tsconfig.aot.json",
+    entryModule: path.resolve(__dirname, "./src/app/app.module#AppModule"),
+});
+//Temporary hack to fix issue with AOT on Windows
+//https://github.com/angular/angular-cli/issues/5329
+aotPlugin._compilerHost._resolve = function(path_to_resolve) {
+    path_1 = require("path");
+    path_to_resolve = aotPlugin._compilerHost._normalizePath(path_to_resolve);
+    if (path_to_resolve[0] == '.') {
+        return aotPlugin._compilerHost._normalizePath(path_1.join(aotPlugin._compilerHost.getCurrentDirectory(), path_to_resolve));
+    }
+    else if (path_to_resolve[0] == '/' || path_to_resolve.match(/^\w:\//)) {
+        return path_to_resolve;
+    }
+    else {
+        return aotPlugin._compilerHost._normalizePath(path_1.join(aotPlugin._compilerHost._basePath, path_to_resolve));
+    }
+};
+
 module.exports = {
     resolve: {
         extensions: ['.ts', '.js']
@@ -13,10 +33,11 @@ module.exports = {
         filename: 'app.main.js'
     },
     plugins: [
-        new ngToolsWebpack.AotPlugin({
-            mainPath: "src/app/main.ts",
-            tsConfigPath: './tsconfig.aot.json'
-        }),
+        // new ngToolsWebpack.AotPlugin({
+        //     mainPath: "src/app/main.ts",
+        //     tsConfigPath: './tsconfig.aot.json'
+        // }),
+        aotPlugin,
         new webpack.LoaderOptionsPlugin({
             minimize: true,
             debug: false
