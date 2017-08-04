912d03092349ae88d6bd597430932261288b5e14
Dan Wahlin <danwahlin@Dans-iMac.local>
2017-08-04 12:57:56 -0700

    Update packages

--- webpack.config.js
+++ webpack.config.js
@@ -8,19 +8,19 @@ var aotPlugin = new ngToolsWebpack.AotPlugin({
 });
 //Temporary hack to fix issue with AOT on Windows
 //https://github.com/angular/angular-cli/issues/5329
-aotPlugin._compilerHost._resolve = function(path_to_resolve) {
-    path_1 = require("path");
-    path_to_resolve = aotPlugin._compilerHost._normalizePath(path_to_resolve);
-    if (path_to_resolve[0] == '.') {
-        return aotPlugin._compilerHost._normalizePath(path_1.join(aotPlugin._compilerHost.getCurrentDirectory(), path_to_resolve));
-    }
-    else if (path_to_resolve[0] == '/' || path_to_resolve.match(/^\w:\//)) {
-        return path_to_resolve;
-    }
-    else {
-        return aotPlugin._compilerHost._normalizePath(path_1.join(aotPlugin._compilerHost._basePath, path_to_resolve));
-    }
-};
+// aotPlugin._compilerHost._resolve = function(path_to_resolve) {
+//     path_1 = require("path");
+//     path_to_resolve = aotPlugin._compilerHost._normalizePath(path_to_resolve);
+//     if (path_to_resolve[0] == '.') {
+//         return aotPlugin._compilerHost._normalizePath(path_1.join(aotPlugin._compilerHost.getCurrentDirectory(), path_to_resolve));
+//     }
+//     else if (path_to_resolve[0] == '/' || path_to_resolve.match(/^\w:\//)) {
+//         return path_to_resolve;
+//     }
+//     else {
+//         return aotPlugin._compilerHost._normalizePath(path_1.join(aotPlugin._compilerHost._basePath, path_to_resolve));
+//     }
+// };
 
 module.exports = {
     resolve: {
